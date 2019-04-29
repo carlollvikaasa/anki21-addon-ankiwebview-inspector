d2a457d8e7173e25a00a351fb85208fe262daee0
hikaru-y <hkrysg@gmail.com>
2019-04-30 02:09:19 +0900

    update

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+### 2019-xx-xx
+- Renamed this add-on from "Web Developer Tools pane for Anki 2.1" to "AnkiWebView Inspector"
+### 2019-03-19
+- Initial Release
--- web_dev_tools_pane.py
+++ web_dev_tools_pane.py
@@ -5,27 +5,45 @@ from aqt import mw
 from aqt.qt import *
 
 
-TITLE = 'web dev tools'
+TITLE = 'AnkiWebView Inspector'
+CONTEXT_MENU_ITEM_NAME = 'Inspect'
 FONT_SIZE = 12
 QDOCKWIDGET_STYLE = '''
     QDockWidget::title {
         padding-top: 0;
         padding-bottom: 0;
-}
+    }
 '''
+
 
 class WebDevToolsPane(QDockWidget):
     """ dockable web dev tools pane """
 
     def __init__(self, title, parent=None):
-        super().__init__(title, parent)
+        super().__init__('', parent, Qt.SubWindow)
         self.setObjectName(title)
         self.setAllowedAreas(
             Qt.LeftDockWidgetArea|Qt.RightDockWidgetArea|Qt.BottomDockWidgetArea)
-        self.toggleViewAction().setText(f'show/hide "{title}"')
+        
+        self.toggleViewAction().setText(f'Toggle {title}')
         # make the title bar thinner
         self.setStyleSheet(QDOCKWIDGET_STYLE)
         self.web = None
+        self.setup_hooks()
+
+    def setup_hooks(self):
+        # メインウィンドウ起動時にはパネルを閉じておく
+        addHook('profileLoaded', self.hide)
+        # プロファイル切り替え時にwebをdelete
+        addHook('unloadProfile', self.delete_web)
+        addHook('AnkiWebView.contextMenuEvent', self.on_context_menu_event)
+        addHook('EditorWebView.contextMenuEvent', self.on_context_menu_event)
+        addHook('beforeStateChange', self.on_anki_state_change)
+
+    def on_context_menu_event(self, web, menu):
+        menu.addAction(CONTEXT_MENU_ITEM_NAME, lambda: self.setup_web(web.page()))
+
+
 
     def setup_web(self, page):
         if self.web:
@@ -45,6 +63,8 @@ class WebDevToolsPane(QDockWidget):
 
         self.web.page().setInspectedPage(page)
         self.setWidget(self.web)
+        # make sure to dock the pane when displaying
+        self.setFloating(False)
         self.show()
 
     @pyqtSlot()
@@ -55,18 +75,17 @@ class WebDevToolsPane(QDockWidget):
         if self.isHidden():
             self.delete_web()
     
+    def on_anki_unload_profile(self):
+        # See aqt/main.py > AnkiQt/_checkForUnclosedWidgets()
+        self.delete_web()
+        self.hide()
+
+    
     def delete_web(self):
         if self.web:
             self.web.deleteLater()
             self.web = None
-
-
-def on_contextMenuEvent(self, menu, pane):
-    def handler():
-        pane.setup_web(self.page())
-
-    a = menu.addAction(f'{TITLE} - {self.title}')
-    a.triggered.connect(handler)
+    
 
 
 def check_qt_version():
@@ -88,15 +107,6 @@ def main():
 
     mw.addDockWidget(Qt.RightDockWidgetArea, pane)
 
-    # メインウィンドウ起動時にはパネルを閉じておく
-    addHook('profileLoaded', pane.hide)
-    # プロファイル切り替え時にwebをdelete
-    addHook('unloadProfile', pane.delete_web)
-    addHook('AnkiWebView.contextMenuEvent',
-            lambda *args, pane=pane: on_contextMenuEvent(*args, pane))
-    addHook('EditorWebView.contextMenuEvent',
-            lambda *args, pane=pane: on_contextMenuEvent(*args, pane))
-    addHook('beforeStateChange', pane.on_anki_state_change)
 
 
 main()
